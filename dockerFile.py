39af7c918b3b02e010d96f76892c773de4100e8c
Krsn Nikita <nikborowik200394@gmail.com>
2022-09-14 17:59:28 +0500

    dockerFile

--- dockerFile.py
+++ dockerFile.py
@@ -1,1 +1,6 @@
- 
+FROM python:3.10.6-alpine
+WORKDIR /mysite
+COPY ./ /mysite
+RUN apk update && pip install -r /mysite/requirements.txt --no-cache-dir
+EXPOSE 8000
+CWD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
