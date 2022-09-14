ab2b8c2b2527217edbe375e5b2f79e736031d99e
Krsn Nikita <nikborowik200394@gmail.com>
2022-09-14 19:14:07 +0500

    update

--- docs/views.py
+++ docs/views.py
@@ -1,5 +1,4 @@
 from rest_framework.response import Response
-from django.http import HttpResponse, JsonResponse
 from rest_framework.views import APIView
 from docs.serializers import ItemImportSerializer,\
 	ItemUpdateSerializer
